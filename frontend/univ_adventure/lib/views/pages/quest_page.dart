52641b7b08130c419e58d528bd4c3d3ac3c765d0
Nathan CORNELOUP <nathan.corneloup@etudiant.univ-rennes.fr>
2024-04-13 11:52:19 +0200

    Mod: Modification de la gestion des quêtes

--- frontend/univ_adventure/lib/views/pages/quest_page.dart
+++ frontend/univ_adventure/lib/views/pages/quest_page.dart
@@ -1,16 +1,26 @@
-// lib/views/pages/quest_page.dart
 import 'package:flutter/material.dart';
+import '../../models/quests.dart'; // Importez votre fichier de modèle Quest ici
 
 class QuestPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Quests'),
-      ),
-      body: Center(
-        child: Text('Welcome to Quests Page!'),
-      ),
+    return FutureBuilder<List<Quest>>(
+      future: loadQuests(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+          return ListView(
+            children: snapshot.data!.map((quest) => ListTile(
+              title: Text(quest.title),
+              subtitle: Text(quest.description),
+            )).toList(),
+          );
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
     );
   }
 }

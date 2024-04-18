ed1049c3dedd8cf9fe144e0fefa43d4fa73cab39
Nathan CORNELOUP <naytan@protonmail.com>
2024-04-18 10:27:30 +0200

    Add: Ajout du composant Badge

---
+++ frontend/univ_adventure/lib/components/badge.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class BadgeComponent extends StatelessWidget {
+  final String badgeName;
+  final IconData badgeIcon;
+  final Color circleColor;
+  final double scale;
+
+  const BadgeComponent({required this.badgeName, required this.badgeIcon, required this.circleColor, this.scale = 1.0});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(4),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: 32 * scale,
+            height: 32 * scale,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: circleColor,
+            ),
+            child: Icon(
+              badgeIcon,
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(width: 4 * scale),
+          Text(
+            badgeName,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 14 * scale,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
